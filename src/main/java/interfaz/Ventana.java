939995136e396fac5f7e1ad909ce75d554ab67a1
arenisca <vseron2000@gmail.com>
2026-06-08 01:10:06 -0400

    Feat: creación de clase que muestra la ventana de la maquina expendedora

---
+++ src/main/java/interfaz/Ventana.java
@@ -0,0 +1,16 @@
+package interfaz;
+
+import javax.swing.*;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        super();
+        setTitle("Expendedor de comida");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setSize(1080,1350); // el tamaño puede cambiar
+
+        PanelPrincipal panel = new PanelPrincipal();
+        add(panel);
+    }
+}
