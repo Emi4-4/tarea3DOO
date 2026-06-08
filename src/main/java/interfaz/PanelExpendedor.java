cc2f856a9daf4e176f526829dff95ad4017b2a83
arenisca <vseron2000@gmail.com>
2026-06-08 01:12:45 -0400

    Modificación: se añadió mouseListener y paintComponent

--- src/main/java/interfaz/PanelExpendedor.java
+++ src/main/java/interfaz/PanelExpendedor.java
@@ -1,41 +1,48 @@
 package interfaz;
 
-import org.example.Expendedor;
+import org.example.*;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class PanelExpendedor extends JPanel {
     private Expendedor expendedor;
     private JPanel botones;
     private JPanel monedas;
-    private JPanel deposito;
-    private JButton botonCocaCola = new JButton();
-    private JButton botonSprite = new JButton();
-    private JButton botonFanta = new JButton();
-    private JButton botonBonobon = new JButton();
-    private JButton botonSuper8 = new JButton();
-    private JButton botonSnicker = new JButton();
+//    private JPanel deposito;
+//    private JButton botonCocaCola = new JButton();
+//    private JButton botonSprite = new JButton();
+//    private JButton botonFanta = new JButton();
+//    private JButton botonBonobon = new JButton();
+//    private JButton botonSuper8 = new JButton();
+//    private JButton botonSnicker = new JButton();
 
     // debe considerar un numero de productos, dulces y bebidas, para la interfaz
     public PanelExpendedor(int numeroProductos){
         super();
-        expendedor=new Expendedor(numeroProductos);
+        this.expendedor=new Expendedor(numeroProductos);
         this.setLayout(null);
+        this.setBackground(new Color(70, 70, 70));
+        this.setPreferredSize(new Dimension(500, 600));
 
-        botones=new JPanel(new GridLayout(4,1));
-        botones.setBounds(getX(),100,100,100);
-        botones.setVisible(true);
-        this.add(botones);
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                expendedor.rellenarDepositosVacios();
+                repaint();
+            }
+        });
+    }
+    public Expendedor getExpendedor() {
+        return expendedor;
+    }
 
-        monedas=new JPanel(new GridLayout());
-        monedas.setBounds(50,50,50,50);
-        monedas.setBackground(new Color(6, 66, 99));
-        this.add(monedas);
-
-
-
-
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        expendedor.paintComponent(g);
     }
 
 }
