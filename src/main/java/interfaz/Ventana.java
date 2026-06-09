e8bc096d2c95d40ff54d0976ccf0b05e3e11f185
Chamo0312 <lenindiazcabezo03@gmail.com>
2026-06-09 17:57:27 -0400

    fix: Correccion en clases ventana y main

--- src/main/java/interfaz/Ventana.java
+++ src/main/java/interfaz/Ventana.java
@@ -3,8 +3,22 @@ package interfaz;
 import javax.swing.*;
 
 public class Ventana extends JFrame {
-    public Ventana(int DineroInicial){
+    public Ventana(){
         super();
+        int DineroInicial = 5000;
+        String input = JOptionPane.showInputDialog(
+                null, "Ingrese el monto inicial en el monedero:",
+                "Configuracion del comprador",
+                JOptionPane.QUESTION_MESSAGE
+        );
+        try {
+            if (input != null && !input.isEmpty()) {
+                DineroInicial = Integer.parseInt(input);}
+            } catch(NumberFormatException e) {
+                JOptionPane.showMessageDialog(
+                        null, "Cantidad invalida, se usara $5000"
+                );
+            }
         setTitle("Expendedor de comida");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setResizable(false);
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,8 @@
 package org.example;
 import interfaz.Ventana;
-import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args){
-        int DineroInicial = 5000;
-        try {
-            String input = JOptionPane.showInputDialog(
-                    null, "Ingrese el monto inicial del monedero",
-                    "Configuracion del monedero",
-                    JOptionPane.QUESTION_MESSAGE);
-            if (input != null){
-                DineroInicial = Integer.parseInt(input);
-            }
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(
-                    null,"Valor invalido introducido, se usara el monto 5000");
-        }
-        new Ventana(DineroInicial);
+        new Ventana();
     }
 }
